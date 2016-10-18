0ddc80c9defc8dcaee4030671043195ceed51516
the-fool <sketchbang@gmail.com>
2016-10-17 19:23:21 -0500

    fix that build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -e
 
-docker run --rm --env-file .env -v $(pwd):/usr/src/app -w /usr/src/app node:6 npm install && run build:prod
+docker run --rm --env-file .env -v $(pwd):/usr/src/app -w /usr/src/app node:6 npm install && npm run build:prod
 docker-compose build
 docker-compose run --rm django python manage.py migrate
 docker-compose run --rm django make datasets
